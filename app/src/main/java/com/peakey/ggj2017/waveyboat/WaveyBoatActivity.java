1db203d36362819762bacd68830432cb8b221344
Prad Ejner <pejner@peakey.com>
2017-01-20 18:50:46 -0500

    initial commit

---
+++ app/src/main/java/com/peakey/ggj2017/waveyboat/WaveyBoatActivity.java
@@ -0,0 +1,13 @@
+package com.peakey.ggj2017.waveyboat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaveyBoatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wavey_boat_activity);
+    }
+}
