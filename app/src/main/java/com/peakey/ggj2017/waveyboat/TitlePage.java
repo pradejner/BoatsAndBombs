e697e5db3ae81f11f3e3179a0b171255f35b9f72
cswaller <cswaller@umail.iu.edu>
2017-01-21 23:08:39 -0500

    title_page

---
+++ app/src/main/java/com/peakey/ggj2017/waveyboat/TitlePage.java
@@ -0,0 +1,49 @@
+package com.peakey.ggj2017.waveyboat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TitlePage extends Activity {
+
+    private Button score;
+    private Button game;
+    private Button credits;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.title_page);
+        game = (Button) findViewById(R.id.start_button);
+        //game button listener onclick creates intent and goes to game activity
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameIntent = new Intent(getApplicationContext(), WaveyBoatActivity.class);
+                startActivity(gameIntent);
+            }
+        });
+        score = (Button) findViewById(R.id.scores_button);
+        //highscore button listener onclick creates intent and goes to highscore activity
+        score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scoreIntent = new Intent(getApplicationContext(), HighScores.class);
+                startActivity(scoreIntent);
+            }
+        });
+
+        credits = (Button) findViewById(R.id.credits_button);
+
+        credits.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent creditsIntent = new Intent(getApplicationContext(), Credits.class);
+                startActivity(creditsIntent);
+            }
+        });
+    }
+}
